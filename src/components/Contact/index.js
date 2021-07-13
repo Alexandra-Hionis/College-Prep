b6c813679ac1b0c6a1b1e47036d615c7b5604bd0
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2021-07-13 18:44:34 -0400

    Included contact info with icons as well as logo image. made some minor front end changes.Still some final details to fix

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,16 +4,86 @@ import { Row, Col, Container } from "react-bootstrap";
 
 function Contact() {
   return (
-    <Container>
+    <Container className="text-center">
       <Row>
-        <Col sm={8}>
-          <p id="contact-info">
-            SydneyvPeters@gmail.com 484-764-9180 greater philadelphia also
-            virtual consultations available for those who are not in the greater
-            philadelphia area
-          </p>
+        <Col lg={6} className="contact-info">
+          <Row>
+            <Col lg={3}></Col>
+            <Col lg={2}>
+              <img
+                alt="email icon"
+                src={process.env.PUBLIC_URL + "/images/email.png"}
+                width="50px"
+                height="50px"
+                className="d-inline-block align-top icon"
+              />
+            </Col>
+            <Col lg={7}>
+              <div className="contact-info">
+                <a href="mailto:SydneyVPeters@gmail.com">
+                  SydneyVPeters@gmail.com
+                </a>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg={3}></Col>
+
+            <Col lg={2}>
+              <img
+                alt="phone icon"
+                src={process.env.PUBLIC_URL + "/images/phone.png"}
+                width="50px"
+                height="50px"
+                className="d-inline-block align-top icon"
+              />
+            </Col>
+            <Col lg={7}>
+              <div className="contact-info">
+                <a href="tel:4847649180">484-764-9180</a>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg={3}></Col>
+
+            <Col lg={2}>
+              <img
+                alt="linkedin icon"
+                src={process.env.PUBLIC_URL + "/images/linkedin copy.png"}
+                width="50px"
+                height="50px"
+                className="d-inline-block align-top icon"
+              />
+            </Col>
+            <Col lg={7}>
+              <div className="contact-info">
+                <a href="https://www.linkedin.com/in/sydneypeters/">
+                  Sydney Peters LinkedIn
+                </a>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg={3}></Col>
+            <Col lg={2}>
+              <img
+                alt="location icon"
+                src={process.env.PUBLIC_URL + "/images/location.png"}
+                width="50px"
+                height="50px"
+                className="d-inline-block align-top icon"
+              />
+            </Col>
+            <Col lg={7}>
+              <div className="contact-info">
+                <p>Greater Philadelphia Area</p>
+              </div>
+            </Col>
+          </Row>
         </Col>
-        <Col sm={4}>
+
+        <Col lg={6}>
           <img
             alt="img of sydney peters"
             src={process.env.PUBLIC_URL + "/images/logoo.png"}

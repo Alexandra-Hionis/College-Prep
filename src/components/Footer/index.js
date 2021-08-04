debe1f3617b02725b0a8dffe187ec54c5ed89fac
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2021-08-03 21:47:04 -0400

    Updated reac t-bootstrap version. Realized I had MDN bootstrap installed and was using it. Ma de sure to uninstall it and delete it from the footer component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,24 +1,17 @@
 import React from "react";
-import { MDBContainer, MDBFooter } from "mdbreact";
+import { Container } from "react-bootstrap";
 import "./styles.css";
 
 const FooterPage = () => {
   return (
-    <MDBFooter className="font-small pt-4 mt-4 footer">
+    <footer className="font-small pt-4 mt-4 footer">
       <div className="footer-copyright text-center py-3">
-        <MDBContainer fluid>
+        <Container fluid>
           &copy; {new Date().getFullYear()} Copyright:{" "}
-          {/* <a href="https://alexandra-hionis.github.io/"> AH Developments </a> */}
           <a href=""> AH Websites </a>
-          {/* <img
-            href="https://alexandra-hionis.github.io/"
-            src={process.env.PUBLIC_URL + "/images/ah-logo.png"}
-            alt="Alexandra Hionis Developments Logo"
-            style={{ width: "50px", height: "50px" }}
-          /> */}
-        </MDBContainer>
+        </Container>
       </div>
-    </MDBFooter>
+    </footer>
   );
 };
 
